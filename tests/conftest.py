fbee5acc3b84b602dada5fd283f2debfc36a92a0
anixel <45008199+andifg@users.noreply.github.com>
2024-10-13 14:11:50 +0200

    use testcontainer default kafka image for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,8 @@ from typing import AsyncGenerator
 
 import pytest
 import pytest_asyncio
-from kafka import KafkaConsumer, KafkaProducer
-from kafka.errors import NoBrokersAvailable
-from testcontainers.core.container import DockerContainer
-from testcontainers.core.waiting_utils import wait_for_logs  # type: ignore
+from kafka import KafkaProducer
+from testcontainers.kafka import RedpandaContainer
 
 from resize.settings import settings
 
@@ -47,44 +45,11 @@ async def fixture_kafka_service() -> AsyncGenerator[str, None]:
             the timeout period.
     """
 
-    kafka_testcontainer = (
-        DockerContainer("docker.io/bitnami/kafka:3.6")
-        .with_bind_ports(9095, 9095)
-        .with_env("KAFKA_CFG_NODE_ID", "0")
-        .with_env("KAFKA_CFG_PROCESS_ROLES", "controller,broker")
-        .with_env("KAFKA_CFG_CONTROLLER_QUORUM_VOTERS", "0@0.0.0.0:9093")
-        .with_env(
-            "KAFKA_CFG_LISTENERS",
-            "PLAINTEXT://:9092,CONTROLLER://:9093,EXTERNAL://0.0.0.0:9095",
-        )
-        .with_env(
-            "KAFKA_CFG_ADVERTISED_LISTENERS",
-            "PLAINTEXT://:9092,EXTERNAL://127.0.0.1:9095",
-        )
-        .with_env(
-            "KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP",
-            "CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,EXTERNAL:PLAINTEXT",
-        )
-        .with_env("KAFKA_CFG_CONTROLLER_LISTENER_NAMES", "CONTROLLER")
-        .with_env("KAFKA_CFG_INTER_BROKER_LISTENER_NAME", "PLAINTEXT")
-    )
+    with RedpandaContainer() as redpanda:
+        connection = redpanda.get_bootstrap_server()
 
-    with kafka_testcontainer as container:
-        host_ip = container.get_container_host_ip()
-        exposed_port = container.get_exposed_port(9095)
-        bootstrap_server = f"{host_ip}:{exposed_port}"
-        wait_for_logs(
-            container=container, predicate="Kafka Server started", timeout=120
-        )
-
-        yield bootstrap_server
-
-    # port = docker_services.port_for("kafka", 9095)
-    # bootstrap_server = f"{docker_ip}:{port}"
-    # docker_services.wait_until_responsive(
-    #     timeout=30.0, pause=0.1, check=lambda: test_kafka(bootstrap_server)
-    # )
-    # return bootstrap_server
+        print(connection)
+        yield connection
 
 
 @pytest_asyncio.fixture()
@@ -109,28 +74,3 @@ async def init_kakfa(
     monkeypatch.setattr(settings, "kafka_bootstrap_servers", kafka_service)
 
     yield TestKafkaSession(sync_producer=sync_producer)
-
-
-def test_kafka(bootstrap_server: str) -> bool:
-    """Test the connection to a Kafka server.
-
-    Args:
-        bootstrap_server (str): The address of the Kafka bootstrap server.
-
-    Returns:
-        bool: True if the connection is successful, False otherwise.
-    """
-    print("Try kafka connection")
-
-    print("BOOOTSTRAP ", bootstrap_server)
-    try:
-        consumer = KafkaConsumer(
-            group_id="test", bootstrap_servers=[bootstrap_server]
-        )
-        print("Connected to Kafka")
-        consumer.bootstrap_connected()
-        return True
-
-    except (NoBrokersAvailable, ValueError) as e:
-        print(e)
-        return False
